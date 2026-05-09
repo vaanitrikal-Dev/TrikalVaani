b8f7822f224deccea7cc082754d2ed94a0d1c781
vaanitrikal-Dev <vaanitrikal@gmail.com>
2026-05-09 17:39:49 +0530

    Update page.tsx

--- app/[domain]/panchang/page.tsx
+++ app/[domain]/panchang/page.tsx
@@ -15,7 +15,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Link from "next/link";
-import citiesData from "../data/cities.json";
+import citiesData from "../../data/cities.json";
 
 export const revalidate = 86400;
 export const dynamicParams = true;
